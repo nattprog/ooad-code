bd2eb956aa0708646ca23241372bfe601d60e0ef
Nathaniel Bong <nattprog7@gmail.com>
2026-02-13 08:38:38 +0800

    implented a new finescheme enum, handling saving of chosen finescheme, with ability to calculate fine based on chosen finescheme and input violating hours

--- parking_lot_management_system/app/src/main/java/parking_lot_management_system/models/Fine.java
+++ parking_lot_management_system/app/src/main/java/parking_lot_management_system/models/Fine.java
@@ -1,5 +1,19 @@
 package parking_lot_management_system.models;
 
-public class Fine {
+import java.math.BigDecimal;
 
+import parking_lot_management_system.models.enums.FineScheme;
+
+public class Fine {
+  private Vehicle vehicle;
+  private FineScheme fineScheme;
+  private Integer violatingHours;
+  private BigDecimal amount;
+
+  Fine(Vehicle vehicle, FineScheme fineScheme, Integer violatingHours) {
+    this.vehicle = vehicle;
+    this.fineScheme = fineScheme;
+    this.violatingHours = violatingHours;
+    this.amount = this.fineScheme.calculateFine(violatingHours);
+  }
 }
---
+++ parking_lot_management_system/app/src/main/java/parking_lot_management_system/models/enums/FineType.java
@@ -0,0 +1,6 @@
+package parking_lot_management_system.models.enums;
+
+public enum FineType {
+  OVERSTAY,
+  UNAUTHORIZED_RESERVED;
+}
